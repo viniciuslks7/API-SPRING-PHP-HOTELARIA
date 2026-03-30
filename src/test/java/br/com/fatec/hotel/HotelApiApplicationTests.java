a1f9083d39c653e3c6e4029751d1f41b538cf196
viniciuslks7 <vinicius.oliveiratwt@gmail.com>
2026-03-30 08:40:00 -0300

    Initial commit - scaffold Spring Boot project

---
+++ src/test/java/br/com/fatec/hotel/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
